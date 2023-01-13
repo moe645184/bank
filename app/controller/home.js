c2ef61d8e892249a6f4a845a406633be79c7a51a
moe645184 <108288076+moe645184@users.noreply.github.com>
2023-01-13 15:36:18 +0800

    初代版

--- app/controller/home.js
+++ app/controller/home.js
@@ -7,7 +7,7 @@ class BankController extends Controller {
     const { ctx } = this;
     // Get the value from Redis
     const value = await ctx.app.redis.zrange('bb',0,10);
-    await ctx.render('index.pug', { value });
+    ctx.body = '泥嚎，請去http:localhost:7001/bank'
   }
 }
 
